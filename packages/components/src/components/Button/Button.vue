c358c43bdefa86cef0ddae5b3138aaa452318dcd
gjssss <gaojinsong2010@gmail.com>
2023-07-27 01:37:26 +0800

    update: 初步完成Button Demo

--- packages/components/src/components/Button/Button.vue
+++ packages/components/src/components/Button/Button.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
+import type { TouchEvent } from '@uni-helper/uni-app-types'
+import { computed } from 'vue'
+import '@dada-element/style/src/Button.scss'
+
+export interface ButtonProps {
+  type?: 'default' | 'primary' | 'secondary' | 'warning' | 'info'
+}
+export interface ButtonEmits {
+  (e: 'click', event: TouchEvent): void
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  type: 'default',
+})
+
+const emit = defineEmits<ButtonEmits>()
+
+function click(event: TouchEvent) {
+  emit('click', event)
+}
+
+const classAry = computed(() => {
+  const { type } = props
+  return [
+    `__dd-button-type-${type}`,
+  ]
+})
 </script>
 
 <template>
-  <div>
-    1233
+  <div class="dada-element-wapper __dd-button" :class="classAry" @click="click">
+    <slot />
   </div>
 </template>
-
-<style></style>
--- packages/components/src/pages/index.vue
+++ packages/components/src/pages/index.vue
@@ -3,8 +3,19 @@
 
 <template>
   <div>
-    123
-    <DadaButton />
+    <DadaButton>按钮</DadaButton>
+    <DadaButton type="primary">
+      按钮
+    </DadaButton>
+    <DadaButton type="secondary">
+      按钮
+    </DadaButton>
+    <DadaButton type="warning">
+      按钮
+    </DadaButton>
+    <DadaButton type="info">
+      按钮
+    </DadaButton>
   </div>
 </template>
 
