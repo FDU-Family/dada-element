0d9f5da24d119feffeb5b09d85d398c093b5af71
gjssss <gaojinsong2010@gmail.com>
2023-07-28 00:53:20 +0800

    fix: 修复字体按钮接口与样式, close #1

--- packages/components/src/pages/index.vue
+++ packages/components/src/pages/index.vue
@@ -3,32 +3,50 @@
 
 <template>
   <div>
-    <DadaButton>按钮</DadaButton>
-    <DadaButton type="primary">
-      按钮
-    </DadaButton>
-    <DadaButton type="secondary">
-      按钮
-    </DadaButton>
-    <DadaButton type="warning">
-      按钮
-    </DadaButton>
-    <DadaButton type="info" :width="100">
-      按钮
-    </DadaButton>
-    <DadaButton :boundless="true" text="warning" :width="200">
-      点击查看
-    </DadaButton>
-    <DadaButton>
-      <view class="w-btn">
+    <h1>正常按钮</h1>
+    <div>
+      <DadaButton>按钮</DadaButton>
+      <DadaButton type="primary">
         按钮
-      </view>
-    </DadaButton>
+      </DadaButton>
+      <DadaButton type="secondary">
+        按钮
+      </DadaButton>
+      <DadaButton type="warning">
+        按钮
+      </DadaButton>
+      <DadaButton type="info">
+        按钮
+      </DadaButton>
+      <DadaButton type="info" text-color="red">
+        按钮
+      </DadaButton>
+      <DadaButton :width="80">
+        按钮
+      </DadaButton>
+    </div>
+    <h1>文字按钮</h1>
+    <div>
+      <DadaButton text>
+        按钮
+      </DadaButton>
+      <DadaButton type="primary" text>
+        按钮
+      </DadaButton>
+      <DadaButton type="secondary" text>
+        按钮
+      </DadaButton>
+      <DadaButton type="warning" text>
+        按钮
+      </DadaButton>
+      <DadaButton type="info" text>
+        按钮
+      </DadaButton>
+      <DadaButton text text-color="red">
+        按钮
+      </DadaButton>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.w-btn {
-  width: 400px;
-}
-</style>
+<style lang="scss" scoped></style>
