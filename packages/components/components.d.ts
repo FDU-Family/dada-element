49396df035fe24da9a7635266269ea19dd0b7004
gjssss <gaojinsong2010@gmail.com>
2023-07-28 20:02:01 +0800

    fix: input基本样式与v-model

--- packages/components/components.d.ts
+++ packages/components/components.d.ts
@@ -10,11 +10,11 @@ declare module 'vue' {
     Button: typeof import('./src/components/Button/Button.vue')['default']
     Circle: typeof import('./src/components/Button/demo/circle.vue')['default']
     DadaButton: typeof import('@/components/Button/Button.vue')['default']
+    DadaInput: typeof import('@/components/Input/Input.vue')['default']
+    Default: typeof import('./src/components/Input/demo/default.vue')['default']
+    Input: typeof import('./src/components/Input/Input.vue')['default']
     Size: typeof import('./src/components/Button/demo/size.vue')['default']
     Text: typeof import('./src/components/Button/demo/text.vue')['default']
     Type: typeof import('./src/components/Button/demo/type.vue')['default']
-
-    Input: typeof import('./src/components/Input/Input.vue')['default']
-    DadaInput: typeof import('@/components/Input/Input.vue')['default']
   }
 }
--- packages/components/src/components/Input/demo/default.vue
+++ packages/components/src/components/Input/demo/default.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const value = ref('123')
 </script>
 
 <template>
-  <DadaInput :width="500" :focus="true" />
+  <div>
+    <div class="mt">
+      <DadaInput v-model:value="value" placeholder="芝士输入框" />{{ value }}
+    </div>
+    <div class="mt">
+      <DadaInput shadow placeholder="芝士输入框" />
+    </div>
+    <div class="mt">
+      <DadaInput border placeholder="芝士输入框" />
+    </div>
+  </div>
 </template>
 
 <style>
-
+.mt {
+  margin-top: 30px;
+}
 </style>
