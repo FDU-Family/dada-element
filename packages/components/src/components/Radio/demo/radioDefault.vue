1253b16a7195c38ed1766bcfcb4a593ae15e719d
kcheng <merlinsbeard@yaocheng.cn>
2023-08-02 18:01:40 +0800

    feat: 添加radio组件库

---
+++ packages/components/src/components/Radio/demo/radioDefault.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const radioAry = [{
+  value: 'USA',
+  name: '美国',
+  checked: 'true',
+},
+{
+  value: 'CHN',
+  name: '中国',
+},
+{
+  value: 'BRA',
+  name: '巴西',
+},
+{
+  value: 'ENG',
+  name: '英国',
+},
+{
+  value: 'FRA',
+  name: '法国',
+},
+]
+</script>
+
+<template>
+  <DadaRadio :range="radioAry" direction="row" type="primary" />
+  <DadaRadio :range="radioAry" direction="column" type="default" />
+</template>
