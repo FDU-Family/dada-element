3fdbbcb96bbb58d55d1543ec41831a6af7a88c0e
chengkai <2557075675@qq.com>
2023-08-03 17:02:44 +0800

    feat: 完善radio功能

--- packages/components/src/components/Radio/demo/radioDefault.vue
+++ packages/components/src/components/Radio/demo/radioDefault.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 const radioAry = [{
   value: 'USA',
   name: '美国',
@@ -21,9 +23,21 @@ const radioAry = [{
   name: '法国',
 },
 ]
+const radioValue = ref(0)
+
+const dadaRadio = ref()
+
+function getRadioValue() {
+  radioValue.value = dadaRadio.value.radioValue()
+}
 </script>
 
 <template>
   <DadaRadio :range="radioAry" direction="row" type="primary" />
   <DadaRadio :range="radioAry" direction="column" type="default" />
+  <DadaRadio ref="dadaRadio" :range="radioAry" direction="row" />
+  <DadaButton @click="getRadioValue">
+    获取radioValue
+  </DadaButton>
+  <text>radioValue的值是: {{ radioAry[radioValue].value }}</text>
 </template>
