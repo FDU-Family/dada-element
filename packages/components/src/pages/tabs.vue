db3d0327a26a30754a227e69ecf49e868c99d84d
gjssss <gaojinsong2010@gmail.com>
2023-08-03 11:43:00 +0800

    feat: 添加tabs组件

---
+++ packages/components/src/pages/tabs.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <div class="wa mt">
+      <tabsBase />
+    </div>
+    <div class="wa mt">
+      <tabsType />
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
