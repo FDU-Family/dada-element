6febe4b8c211efb52dffdfad806d84c6882a8098
gjssss <gaojinsong2010@gmail.com>
2023-08-08 20:56:34 +0800

    fix: pop out animation, close #11

--- packages/components/src/components/PopOut/PopOut.vue
+++ packages/components/src/components/PopOut/PopOut.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import '@dada-element/style/src/PopOut.scss'
-import { computed, defineEmits, defineProps, withDefaults } from 'vue'
+import { defineEmits, defineProps, ref, watch, watchEffect, withDefaults } from 'vue'
 
 export interface PopOutProps {
   visible?: boolean
@@ -20,11 +20,50 @@ const props = withDefaults(defineProps<PopOutProps>(), {
 
 const emits = defineEmits<PopOutEmits>()
 
-const classAry = computed(() => {
-  const { mask } = props
-  return [
-    mask ? '__dd-pop-out-mask' : '',
-  ]
+const isAnimate = ref(false)
+let timer: null | NodeJS.Timeout = null
+
+watch(() => props.visible, (newVal) => {
+  if (newVal) {
+    // true，表示显示弹出框
+    isAnimate.value = true // 开启动画
+    if (timer)
+      clearTimeout(timer)
+  }
+  else {
+    // false
+    timer = setTimeout(() => {
+      isAnimate.value = false // 关闭动画
+    }, 300)
+  }
+})
+const maskClassObj = ref<Record<string, boolean>>({})
+const classObj = ref<Record<string, boolean>>({})
+
+watchEffect(() => {
+  const { mask, visible } = props
+  maskClassObj.value['__dd-pop-out-mask'] = mask
+  maskClassObj.value['__dd-pop-out-mask-from'] = mask
+  if (visible) {
+    setTimeout(() => {
+      maskClassObj.value['__dd-pop-out-mask-to'] = true
+    }, 50)
+  }
+  else {
+    delete maskClassObj.value['__dd-pop-out-mask-to']
+  }
+})
+
+watchEffect(() => {
+  const { visible } = props
+  if (visible) {
+    setTimeout(() => {
+      classObj.value['__dd-pop-out-to'] = true
+    }, 50)
+  }
+  else {
+    delete classObj.value['__dd-pop-out-to']
+  }
 })
 
 function clickHandle() {
@@ -34,9 +73,9 @@ function clickHandle() {
 </script>
 
 <template>
-  <div v-if="visible" class="dada-element-wrapper  __dd-pop-out-container" @click="clickHandle">
-    <div :class="classAry" />
-    <div class="__dd-pop-out">
+  <div v-if="visible || isAnimate" class="dada-element-wrapper  __dd-pop-out-container" @click="clickHandle">
+    <div :class="maskClassObj" />
+    <div class="__dd-pop-out __dd-pop-out-from" :class="classObj">
       <slot />
     </div>
   </div>
