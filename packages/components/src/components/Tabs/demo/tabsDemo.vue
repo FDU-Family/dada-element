702380db74f0606470908637a630784c7ba7bd8b
chengkai <2557075675@qq.com>
2023-10-27 11:56:04 +0800

    feat: 完成动态options

--- packages/components/src/components/Tabs/demo/tabsDemo.vue
+++ packages/components/src/components/Tabs/demo/tabsDemo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import type { tabsOptionsType } from '../../../types.ts'
 
 const options: tabsOptionsType = [
@@ -19,6 +19,42 @@ const options: tabsOptionsType = [
   },
 ]
 const index = ref(0)
+
+const flag = ref(false)
+
+const testOptions = computed(() => {
+  const baseOptions = [
+    {
+      lable: '活动信息',
+      key: 'activityInfo',
+    },
+
+    {
+      lable: '评论(10)',
+      key: 'comment',
+    },
+  ]
+  const applyOptions = [
+    {
+      lable: '活动信息',
+      key: 'activityInfo',
+    },
+    {
+      lable: '队伍信息',
+      key: 'teamInfo',
+    },
+    {
+      lable: '评论(10)',
+      key: 'comment',
+    },
+  ]
+
+  return flag.value === false ? baseOptions : applyOptions
+})
+
+function updateOptions() {
+  flag.value = !flag.value
+}
 </script>
 
 <template>
@@ -27,6 +63,29 @@ const index = ref(0)
       v-model:value的值{{ index }} 点击＋1
     </div>
     <DadaTabs v-model:value="index" :tabs-options="options">
+      <template #slot-0>
+        <div>
+          activity
+        </div>
+      </template>
+      <template #slot-1>
+        <div>
+          team
+        </div>
+      </template>
+      <template #comment>
+        <div>
+          comment
+        </div>
+      </template>
+    </DadaTabs>
+  </div>
+
+  <div>
+    <div @click="updateOptions">
+      测试dot动态变化
+    </div>
+    <DadaTabs v-model:value="index" :tabs-options="testOptions">
       <template #slot-0>
         <div>
           activity
